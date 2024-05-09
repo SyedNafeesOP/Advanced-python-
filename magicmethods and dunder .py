bb70737ebab35a554b2e31809b37f9e8fe0cf9eb
SyedNafeesOP <syednafeesop.2000@gmail.com>
2024-05-09 01:27:32 -0700

    Learning Magic methods

--- magicmethods and dunder .py
+++ magicmethods and dunder .py
@@ -1,5 +1,6 @@
 # In Python, "magic methods" are special methods with double underscores (also called "dunder" methods, short for "double underscore"). These methods allow you to define how objects of your class behave in certain situations. They are called "magic" because they are invoked automatically behind the scenes in response to certain operations on objects.
 
+
 class Mobile:
     def __init__(self,name,Ram,Processor):
         self.name=name
@@ -9,3 +10,41 @@ M1=Mobile("Samsung galaxy a15",8,"Octa-core Dimensity 720")
 print(M1.name)
 print(M1.Ram)
 print(M1.Processor)
+
+
+class Practical:
+    def __init__(self, name):
+        self.name = name
+
+class Person(Practical):
+    def __init__(self, names):
+        super().__init__(names)
+
+obj = Practical(['Chemistry', 'Physics', 'Computer'])
+for subject in obj.name:
+    print(subject)
+print(obj.name)
+
+obj1 = Person(["Nafees", "Hasnat", "Zohaib", "Farhan", "Umair"])
+for name in obj1.name:
+    print(name)
+print(obj1.name)
+
+# __init__:
+# Create a class representing a book. Implement the __init__ method to initialize attributes such as title, author, and pages.
+
+class Book:
+    def __init__(self,title,author,pages):
+        self.title=title
+        self.author=author
+        self.pages=pages
+
+BOOK1=Book(
+    "The Hitchhiker's Guide to the Galaxy",
+    "Douglas Adams", 
+    215
+)
+        
+print(BOOK1.title)
+print(BOOK1.author)
+print(BOOK1.pages)
